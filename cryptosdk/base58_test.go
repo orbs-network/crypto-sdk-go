5fe4ccaa95847568c143b26988a4f92fca4d995b
Ido Zilberberg <idoz@orbs.com>
2018-07-04 13:57:51 +0300

    Added impl & test for ED25519Key

---
+++ cryptosdk/base58_test.go
@@ -0,0 +1,19 @@
+package cryptosdk_test
+
+import (
+	"github.com/orbs-network/crypto-sdk-go/cryptosdk"
+	"fmt"
+	"encoding/hex"
+	"testing"
+)
+
+func TestEd25519key(t *testing.T) {
+	inst := cryptosdk.ED25519KeyNew()
+	defer inst.ED25519KeyFree()
+	fmt.Printf("Instance: %T %v\n", inst, inst)
+	fmt.Printf("Instance has priv key: %v\n", inst.ED25519KeyHasPrivateKey())
+	publicKey := inst.ED25519KeyGetPublicKey()
+	publicKeyStr := hex.EncodeToString(publicKey)
+	fmt.Printf("Public Key: %T %v\n", publicKey, publicKey)
+	fmt.Printf("Public Key str: %T %v\n", publicKeyStr, publicKeyStr)
+}
